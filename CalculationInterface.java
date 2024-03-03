09cb3424acef5c16ee3cb370736ac7ca0dda675c
Maissa Dridi <maissadridi1@gmail.com>
2024-03-03 12:34:41 +0100

    interface creation

---
+++ CalculationInterface.java
@@ -0,0 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CalculationInterface extends Remote {
+    double add(double x, double y) throws RemoteException;
+
+    double subtract(double x, double y) throws RemoteException;
+
+    double multiply(double x, double y) throws RemoteException;
+
+    double divide(double x, double y) throws RemoteException;
+}
